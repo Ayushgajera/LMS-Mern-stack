cab273dd70b5c18219d5908c63564b102e00e02e
ayush gajera <ayushgajera50@gmail.com>
2025-04-07 22:00:48 +0530

    login controller

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -41,6 +41,8 @@ export const login = async (req, res) => {
 
     try {
         const { email, password } = req.body;
+        console.log(email, password);
+        
         if (!email || !password) {
             return res.status(400).json({
                 success: false,
